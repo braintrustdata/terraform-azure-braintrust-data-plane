1b8cf89ea66cbee1e28ee99b20ec455a586b2f22
Mike Deeks <mike@braintrustdata.com>
2025-02-28 09:30:04 -0800

    Snapshot

--- main.tf
+++ main.tf
@@ -12,43 +12,22 @@ locals {
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = "rg-${var.deployment_name}"
+  name     = var.deployment_name
   location = var.location
-
-  tags = {
-    deployment = var.deployment_name
-  }
 }
 
 module "main_vnet" {
   source = "./modules/vnet"
 
-  deployment_name     = var.deployment_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = var.location
-  vnet_name           = "main"
-  vnet_address_space  = var.vnet_address_space
-
-  public_subnet_cidr    = cidrsubnet(var.vnet_address_space, 8, 0)
-  private_subnet_1_cidr = cidrsubnet(var.vnet_address_space, 8, 1)
-  private_subnet_2_cidr = cidrsubnet(var.vnet_address_space, 8, 2)
-  private_subnet_3_cidr = cidrsubnet(var.vnet_address_space, 8, 3)
-}
-
-module "quarantine_vnet" {
-  source = "./modules/vnet"
-  count  = var.enable_quarantine_vnet ? 1 : 0
-
-  deployment_name     = var.deployment_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = var.location
-  vnet_name           = "quarantine"
-  vnet_address_space  = var.quarantine_vnet_address_space
-
-  public_subnet_cidr    = cidrsubnet(var.quarantine_vnet_address_space, 8, 0)
-  private_subnet_1_cidr = cidrsubnet(var.quarantine_vnet_address_space, 8, 1)
-  private_subnet_2_cidr = cidrsubnet(var.quarantine_vnet_address_space, 8, 2)
-  private_subnet_3_cidr = cidrsubnet(var.quarantine_vnet_address_space, 8, 3)
+  deployment_name         = var.deployment_name
+  resource_group_name     = azurerm_resource_group.main.name
+  location                = var.location
+  vnet_name               = "main"
+  vnet_address_space_cidr = var.vnet_address_space_cidr
+  database_subnet_cidr    = var.database_subnet_cidr
+  services_subnet_cidr    = var.services_subnet_cidr
+  gateway_subnet_cidr     = var.gateway_subnet_cidr
+  redis_subnet_cidr       = var.redis_subnet_cidr
 }
 
 module "database" {
@@ -82,49 +61,4 @@ module "redis" {
   redis_family        = var.redis_family
   redis_capacity      = var.redis_capacity
   redis_version       = var.redis_version
-}
-
-module "services" {
-  source = "./modules/services"
-
-  deployment_name     = var.deployment_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = var.location
-
-  # Data stores
-  postgres_username = module.database.postgres_database_username
-  postgres_password = module.database.postgres_database_password
-  postgres_host     = module.database.postgres_database_fqdn
-  postgres_port     = module.database.postgres_database_port
-  redis_host        = module.redis.redis_hostname
-  redis_port        = module.redis.redis_port
-  redis_password    = module.redis.redis_primary_key
-
-  # Service configuration
-  braintrust_org_name                = var.braintrust_org_name
-  api_handler_scale_out_capacity     = var.api_handler_scale_out_capacity
-  whitelisted_origins                = var.whitelisted_origins
-  outbound_rate_limit_window_minutes = var.outbound_rate_limit_window_minutes
-  outbound_rate_limit_max_requests   = var.outbound_rate_limit_max_requests
-  custom_domain                      = var.custom_domain
-  custom_certificate_id              = var.custom_certificate_id
-
-  # Networking
-  vnet_id = module.main_vnet.vnet_id
-  subnet_ids = [
-    module.main_vnet.private_subnet_1_id,
-    module.main_vnet.private_subnet_2_id,
-    module.main_vnet.private_subnet_3_id
-  ]
-
-  # Quarantine VNet
-  use_quarantine_vnet = var.enable_quarantine_vnet
-  quarantine_vnet_id  = var.enable_quarantine_vnet ? module.quarantine_vnet[0].vnet_id : null
-  quarantine_vnet_subnet_ids = var.enable_quarantine_vnet ? [
-    module.quarantine_vnet[0].private_subnet_1_id,
-    module.quarantine_vnet[0].private_subnet_2_id,
-    module.quarantine_vnet[0].private_subnet_3_id
-  ] : []
-
-  key_vault_id = local.key_vault_id
 }
