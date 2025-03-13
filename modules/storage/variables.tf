4ac4e69b8993639568a3e47b9c2a12b2b2d5f602
Mike Deeks <mike@braintrustdata.com>
2025-03-13 11:27:42 -0700

    Add support for blob storage

---
+++ modules/storage/variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the storage account"
+  type        = string
+}
+
+variable "deployment_name" {
+  description = "The name of the deployment. Used for naming resources."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure Region in which to create the storage account"
+  type        = string
+}
+
+variable "key_vault_id" {
+  description = "The ID of the Key Vault to use for encryption"
+  type        = string
+}
+
+variable "private_endpoint_subnet_id" {
+  description = "The ID of the subnet to use for the private endpoint"
+  type        = string
+}
+
+variable "vnet_id" {
+  description = "The ID of the virtual network to link with the private DNS zone"
+  type        = string
+}
