77917206b2af424ff062cfdd85864a10ebd64753
Mike Deeks <mike@braintrustdata.com>
2025-04-25 16:08:20 -0700

    Refer to vnet with index since it is conditional

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ module "storage" {
   resource_group_name        = azurerm_resource_group.main.name
   deployment_name            = var.deployment_name
   location                   = var.location
-  vnet_id                    = module.main_vnet.vnet_id
-  private_endpoint_subnet_id = module.main_vnet.private_endpoint_subnet_id
+  vnet_id                    = module.main_vnet[0].vnet_id
+  private_endpoint_subnet_id = module.main_vnet[0].private_endpoint_subnet_id
   key_vault_id               = local.key_vault_id
 }
