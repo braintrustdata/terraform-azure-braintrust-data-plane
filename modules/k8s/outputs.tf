e7622a048c2a1b934cc243f90eed582d5849a915
Mike Deeks <mike@braintrustdata.com>
2025-06-17 14:21:42 -0700

    Output the cluster and kube_config

--- modules/k8s/outputs.tf
+++ modules/k8s/outputs.tf
@@ -1,4 +1,8 @@
-output "aks" {
-  value     = azurerm_kubernetes_cluster.aks
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
 }
